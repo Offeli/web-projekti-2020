9800765f69587db7e2cfcf9f6e8d5894e1b0700e
Offeli <oskari.narhi@outlook.com>
2020-06-01 20:42:17 +0300

    Done for now

---
+++ src/components/AddingForm.vue
@@ -0,0 +1,134 @@
+<template>
+    <div id="AddingForm">
+        <form @submit.prevent="handleSubmit">
+            <p>
+                <label> Review: </label>
+                <input
+                        class="review-area"
+                        type="text"
+                        :class="{ 'has-error': submitting && invalidComment }"
+                        v-model="review.comment"
+                        @focus="clearStatus"
+                        @keypress="clearStatus"
+                />
+            </p>
+
+            <p>
+                <label> Age suggestion: </label>
+                <input
+                        type="text"
+                        :class="{ 'has-error': submitting && invalidAge }"
+                        v-model="review.age"
+                        @focus="clearStatus"
+                        @keypress="clearStatus"
+                />
+            </p>
+
+            <p>
+                <label > Rating: </label>
+                <select id="score" v-model="review.score">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+
+            <p v-if="error && submitting" class="error-message">
+                ❗Please fill out all required fields
+            </p>
+            <p v-if="success" class="success-message">
+                ✅ Review successfully added
+            </p>
+            <p/>
+            <button>Add Review</button>
+        </form>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'AddingForm',
+    data() {
+      return {
+        submitting: false,
+        error: false,
+        success: false,
+        review: {
+          score: ' ',
+          age: ' ',
+          comment: ' ',
+        }
+      }
+    },
+    methods: {
+      handleSubmit() {
+        this.submitting = true;
+        this.clearStatus();
+
+        if (this.invalidComment || this.invalidScore || this.invalidAge) {
+          this.error = true;
+          return
+        }
+
+        this.$emit('add:ctrReview', this.review);
+        this.$emit('add:haloReview', this.review);
+        this.$emit('add:civReview', this.review);
+        this.$refs.first.focus();
+
+        this.review = {
+          score: ' ',
+          age: ' ',
+          comment: ' ',
+        };
+        this.error = false;
+        this.success = true;
+        this.submitting = false
+      },
+
+      clearStatus() {
+        this.success = false;
+        this.error = false
+      }
+    },
+    computed: {
+      invalidComment() {
+        return this.review.comment === ''
+      },
+
+      invalidScore() {
+        return this.review.score === ''
+      },
+
+      invalidAge() {
+        return this.review.age === ''
+      },
+    },
+  }
+</script>
+
+<style scoped>
+    form {
+        margin: 1rem;
+        border: double;
+        padding: 1rem;
+    }
+
+    [class*='-message'] {
+        font-weight: 500;
+    }
+    .review-area{
+        height: 5rem;
+        width: 20rem;
+    }
+
+
+    .error-message {
+        color: #d33c40;
+    }
+
+    .success-message {
+        color: #32a95d;
+    }
+</style>
---
+++ src/components/GameTable.vue
@@ -0,0 +1,99 @@
+<template>
+<div id="GameTable">
+    <table>
+        <thead>
+        <tr>
+            <th>Game name</th>
+            <th>Reviews</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="game in games" :key="game.id">
+            <td >{{game.name}}</td>
+            <td v-if="game.id === 1">
+                <button @click="swapCtr">See reviews</button>
+                <template v-if="ctrSeen">
+                <span v-for="ctrReview in ctrReviews" :key="ctrReview.id" class="reviews">
+                    <br>
+                    Age recommendation: {{ ctrReview.age }},
+                    Score: {{ctrReview.score}},
+                    Comment: {{ctrReview.comment}}
+                </span>
+                </template>
+            </td>
+            <td v-else-if="game.id === 2">
+                <button @click="swapHalo">See reviews</button>
+                <template v-if="haloSeen">
+                <span v-for="haloReview in haloReviews" :key="haloReview.id"  class="reviews">
+                    <br>
+                    Age recommendation: {{ haloReview.age }},
+                    Score: {{haloReview.score}},
+                    Comment: {{haloReview.comment}}
+                </span>
+                </template>
+            </td>
+            <td v-else-if="game.id === 3">
+                <button @click="swapCiv">See reviews</button>
+                <template v-if="civSeen">
+                <span v-for="civReview in civReviews" :key="civReview.id"  class="reviews">
+                    <br>
+                    Age recommendation: {{ civReview.age }},
+                    Score: {{civReview.score}},
+                    Comment: {{civReview.comment}}
+                </span>
+                </template>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'GameTable',
+    props: {
+      games: Array,
+      ctrReviews: Array,
+      haloReviews: Array,
+      civReviews: Array
+    },
+    data() {
+      return {
+        ctrSeen: false,
+        haloSeen: false,
+        civSeen: false
+      }
+    },
+    methods: {
+      swapCtr: function(){
+        if (this.ctrSeen === true){
+          this.ctrSeen = false
+        } else if (this.ctrSeen === false){
+          this.ctrSeen = true
+        }
+      },
+      swapHalo: function(){
+        if (this.haloSeen === true){
+          this.haloSeen = false
+        } else if (this.haloSeen === false){
+          this.haloSeen = true
+        }
+      },
+      swapCiv: function(){
+        if (this.civSeen === true){
+          this.civSeen = false
+        } else if (this.civSeen === false){
+          this.civSeen = true
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    button {
+        margin: 0 0.5rem 0 0;
+    }
+</style>
